15cae65966c1c788eb5fc3af4b2c550cd403cc1e
Webduino <volkovaelena19730415@gmail.com>
2025-03-09 16:44:10 +0800

    Update whalesjavas.js

--- whalesjavas.js
+++ whalesjavas.js
@@ -39,3 +39,11 @@ window.onload = year;
 setTimeout(function(){
 	document.body.classList.add('body_visible');
 }, 200);
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const navList = document.getElementById('nav-list');
+
+    menuIcon.addEventListener('click', function () {
+        navList.classList.toggle('active'); 
+    });
+});
